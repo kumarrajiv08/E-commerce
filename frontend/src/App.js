34bded1fd8fded76f9ce90c92831cf1a18885c35
Rajiv kumar <rajivkumarbna@gmail.com>
2021-04-29 16:28:04 +0530

    homescreen product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
+import HomeScreen from './screens/HomeScreen.js'
+
 const App = () => {
   return (
     <>
     <Header />
     <main className = 'py-3'>
     <Container>
-      <h1>Welcome to Proshop </h1>
+
+      <HomeScreen />
       </Container>
     </main>
     <Footer />
-
+  
     </>
   );
 }
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Col, Row} from 'react-bootstrap'
+import Product from '../components/Product'
+import products from '../products.js'
+const HomeScreen = () => {
+    return (
+        <>
+        <h1>LATEST PRODUCTS</h1>
+          <Row>
+            {products.map(product =>(
+              <Col sm={12} md={6} lg={4} xl={3}>   {/*In small screen, it will take full width, in medium it will take half*/}
+                <Product product={product}/>
+
+              </Col>
+            ))}
+          </Row>
+        </>
+    )
+}
+
+export default HomeScreen
