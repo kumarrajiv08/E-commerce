ea6de09bac1ea0513ee3b47c8cee01ac87d983fa
Rajiv kumar <rajivkumarbna@gmail.com>
2021-06-10 15:48:17 +0530

    authentication middleware

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 
 
@@ -13,15 +14,16 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World!...')
 })
 
 app.use('/api/products',productRoutes)
-
+app.use('/api/users',userRoutes)
 
 app.use(notFound)
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
