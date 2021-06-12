320d09cc697efc62492e8ce1560f52fa394ac033
Rajiv kumar <rajivkumarbna@gmail.com>
2021-06-12 21:50:49 +0530

    User login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer.js';
 import HomeScreen from './screens/HomeScreen.js'
 import ProductScreen from './screens/ProductScreen.js'
 import CartScreen from './screens/CartScreen.js'
+import LoginScreen from './screens/LoginScreen.js'
 
 
 const App = () => {
@@ -15,9 +16,10 @@ const App = () => {
     <main className = 'py-3'>
     <Container>
 
-      <Route path ='/' component={HomeScreen} exact />
+      <Route path ='/login' component={LoginScreen}/>
       <Route path ='/product/:id' component={ProductScreen}/>
       <Route path ='/cart/:id?' component={CartScreen}/>
+      <Route path ='/' component={HomeScreen} exact />
       </Container>
     </main>
     <Footer />
