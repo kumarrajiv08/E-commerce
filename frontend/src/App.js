b20ba7556f294968fdd369851f3ab0c96956e5e2
Rajiv kumar <rajivkumarbna@gmail.com>
2021-06-15 14:54:22 +0530

    User register,reducer, action and screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import HomeScreen from './screens/HomeScreen.js'
 import ProductScreen from './screens/ProductScreen.js'
 import CartScreen from './screens/CartScreen.js'
 import LoginScreen from './screens/LoginScreen.js'
+import RegisterScreen from './screens/RegisterScreen'
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
     <Container>
 
       <Route path ='/login' component={LoginScreen}/>
+      <Route path='/register' component={RegisterScreen}/>
       <Route path ='/product/:id' component={ProductScreen}/>
       <Route path ='/cart/:id?' component={CartScreen}/>
       <Route path ='/' component={HomeScreen} exact />
