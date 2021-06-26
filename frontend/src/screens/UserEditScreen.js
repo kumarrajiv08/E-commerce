3a6f38252a93c6d65f7cc2eb6633ce7a5c3bab39
Rajiv kumar <rajivkumarbna@gmail.com>
2021-06-26 11:25:24 +0530

    update user functionality

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUser } from '../actions/userActions'
+import { USER_UPDATE_RESET } from '../constants/userConstants'
 
 const UserEditScreen = ({ match, history }) => {
   const userId = match.params.id
@@ -19,7 +20,20 @@ const UserEditScreen = ({ match, history }) => {
   const userDetails = useSelector((state) => state.userDetails)
   const { loading, error, user } = userDetails
 
+  const userUpdate = useSelector((state) => state.userUpdate)
+const {
+  loading: loadingUpdate,
+  error: errorUpdate,
+  success: successUpdate,
+} = userUpdate
+
+
   useEffect(() => {
+
+    if (successUpdate) {
+    dispatch({ type: USER_UPDATE_RESET })
+    history.push('/admin/userlist')
+  } else {
     if (!user.name || user._id !== userId) {
       dispatch(getUserDetails(userId))
     } else {
@@ -27,10 +41,13 @@ const UserEditScreen = ({ match, history }) => {
       setEmail(user.email)
       setIsAdmin(user.isAdmin)
     }
-  }, [dispatch, userId, user])
+  }
+
+}, [dispatch, history,userId, user,successUpdate])
 
   const submitHandler = (e) => {
     e.preventDefault()
+      dispatch(updateUser({ _id: userId, name, email, isAdmin }))
   }
 
   return (
@@ -40,6 +57,8 @@ const UserEditScreen = ({ match, history }) => {
       </Link>
       <FormContainer>
         <h1>Edit User</h1>
+          {loadingUpdate && <Loader />}
+         {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
         {loading ? (
           <Loader />
         ) : error ? (
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer,userDetailsReducer,userUpdateProfileReducer , userListReducer,userDeleteReducer,} from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer,userDetailsReducer,userUpdateProfileReducer , userListReducer,userDeleteReducer, userUpdateReducer,} from './reducers/userReducers'
 import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer,} from './reducers/orderReducers'
 const reducer = combineReducers({
   productList: productListReducer,
@@ -15,6 +15,8 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   userList: userListReducer,
    userDelete: userDeleteReducer,
+    userUpdate: userUpdateReducer,
+
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
