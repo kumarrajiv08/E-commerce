b32b50525f1c0cc6d9c93462fcae85c266fe95f5
Rajiv kumar <rajivkumarbna@gmail.com>
2021-06-28 21:16:27 +0530

    ceate and update product endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express'
 const router = express.Router()
-import {getProducts,getProductById, deleteProduct,} from '../controllers/productControllers.js'
+import {getProducts,getProductById, deleteProduct,createProduct,
+  updateProduct,} from '../controllers/productControllers.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
-
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
-
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
 
 
