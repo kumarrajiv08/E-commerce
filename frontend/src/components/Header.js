7c87f720464cbaf89f942c80125b1ed4dbde3afc
Rajiv kumar <rajivkumarbna@gmail.com>
2021-09-09 10:19:33 +0530

    changed products

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>WeCARE</Navbar.Brand>
+            <Navbar.Brand>PROSHOP</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
